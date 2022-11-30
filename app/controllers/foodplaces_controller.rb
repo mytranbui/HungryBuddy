2bea16efebc5469343a4391dd9779c53296f72d0
Salome Abramishvili <salomeeabramishvili@gmail.com>
2022-11-30 18:13:27 +0100

    navbar, logo, search

--- app/controllers/foodplaces_controller.rb
+++ app/controllers/foodplaces_controller.rb
@@ -1,7 +1,11 @@
 class FoodplacesController < ApplicationController
   before_action :foodplace_params, only: %(show)
   def index
-    @foodplaces = Foodplace.all
+    if params[:query].present?
+      @foodplaces = Foodplace.where("name ILIKE ?", "%#{params[:query]}%")
+    else
+      @foodplaces = Foodplace.all
+    end
   end
 
   def show
