c7c59c40d282bb0c7552fa55d23efd176946abd9
Salome Abramishvili <salomeeabramishvili@gmail.com>
2022-12-05 13:00:37 +0100

    sgwegsd

--- app/controllers/foodplaces_controller.rb
+++ app/controllers/foodplaces_controller.rb
@@ -16,6 +16,17 @@ class FoodplacesController < ApplicationController
     end
   end
 
+  def map
+    @foodplaces = Foodplace.all
+    @markers = @foodplaces.geocoded.map do |foodplace|
+    {
+      lat: foodplace.latitude,
+      lng: foodplace.longitude,
+      info_window: render_to_string(partial: "info_window", locals: { foodplace: foodplace })
+    }
+    end
+  end
+
   def show
     @review = Review.new
     # @markers = @foodplace.geocoded.map do |foodplace|
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     resources :reviews, only: :create
   end
   # resources :reviews
-
+  get "map", to: "foodplaces#map"
   resources :lists do
     resources :bookmarks, only: [:new, :create]
   end
