4d472b188ed31f90ddd9571920d6248c407463ec
Bishwajit Karmaker <bish.karm123@gmail.com>
2022-12-08 11:01:33 +0100

    foodplaces_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,15 +17,29 @@ Foodplace.destroy_all
 categories = ["restaurant", "bar", "cafe"]
 
 puts 'Creating foodplaces...'
+
 addresses = ["Huttenstraße 7, 10553 Berlin", "Zinnowitzer Str. 5, 10115 Berlin", "Potsdamer Str. 69, 10785 Berlin", \
               "Lützowstraße 19, 10785 Berlin", "Potsdamer Str. 84, 10785 Berlin", "Potsdamer Str. 75, 10785 Berlin", \
               "Potsdamer Str. 85, 10785 Berlin","Potsdamer Str. 99, 10785 Berlin", "Potsdamer Str. 73, 10785 Berlin", \
-                "Eichhornstraße 3, 10785 Berlin"]
+              "Eichhornstraße 3, 10785 Berlin","Kochstraße 16, 10969 Berlin","Friedrichstraße 211, 10969 Berlin",\
+              "Rudi-Dutschke-Straße 25, 10969 Berlin","Charlottenstraße 76, 10117 Berlin","Rudi-Dutschke-Straße 26, 10969 Berlin", \
+              "Zimmerstraße 19, 10969 Berlin","Hedemannstraße 10, 10969 Berlin","Friedrichstraße 200, 10117 Berlin", \
+              "Chupenga Burritos & Salads","Köthener Str. 28, 10963 Berlin"," Rudi-Dutschke-Straße 23, 10969 Berlin", \
+              " Rudi-Dutschke-Straße 20, 10969 Berlin","Charlottenstraße 79-80, 10117 Berlin","Friedrichstraße 12, 10969 Berlin", \
+              "Wilhelmstraße 41A, 10963 Berlin","Rudi-Dutschke-Straße 9, 10969 Berlin","Charlottenstraße 63, 10117 Berlin", \
+              "Charlottenstraße 1, 10969 Berlin"," Leipziger Str. 30, 10117 Berlin","Friedrichstraße 237, 10969 Berlin", \
+              "Kottbusser Str. 1, 10999 Berlin","Kurfürstendamm 160, 10709 Berlin"," Köpenicker Str. 159, 10997 Berlin",
+              "Adalbertstraße 97, 10999 Berlin","Am Ostbahnhof 9, 10243 Berlin","Wrangelstraße 20, 10997 Berlin",\
+              "Admiralstraße 36-37, 10999 Berlin","Erich-Steinfurth-Straße 4, 10243 Berlin","Stralauer Pl. 30-31, 10243 Berlin",\
+              "Kohlfurter Str. 37, 10999 Berlin","Fatih Servet Döner Restaurant im Ostbahnhof Am, Am Ostbahnhof 3, 10243 Berlin"," Lausitzer Pl. 12, 10997 Berlin"\
+              "Alexanderpl. 7, 10178 Berlin","Mildred-Harnack-Straße 11, 10243 Berlin","Mariannenpl. 23, 10997 Berlin",\
+              "Alexanderpl. 2, 10178 Berlin"," Strausberger Pl. 2, 10243 Berlin","Erich-Steinfurth-Straße 9, 10243 Berlin",\
+              "Alexanderplatz, Panoramastraße 1A, 10178 Berlin","Mühlenstraße 5 / auf dem Mercedes-Platz, 10243 Berlin"]
 i = 0
-# User.create(email: "test@gmail.com", password: "123456")
-10.times do
+50.times do
   foodplace = Foodplace.new(name: Faker::Restaurant.name, address: addresses[i], cuisine: Faker::Food.ethnic_category, \
-  phone_number: Faker::PhoneNumber.phone_number_with_country_code , category: categories.sample, website: Faker::Internet.url  , google_rating: rand(1..5), opening_times:"8:00 - 18:00")
+  phone_number: Faker::PhoneNumber.phone_number_with_country_code , category: categories.sample, website: Faker::Internet.url,\
+  google_rating: rand(1..5), opening_times:"9:00 - 22:00")
   query = ["restaurant", "bar", "cafe"].sample
   file = URI.open("https://source.unsplash.com/random/?#{query}")
   foodplace.photo.attach(io: file, filename: "#{foodplace.name}.png", content_type: "image/png")
@@ -33,20 +47,6 @@ i = 0
   i += 1
 end
 
-
-# foodplace = [
-#   {
-#     name: "Joseph-Roth-Diele",
-#     address: " Potsdamer Str. 75, 10785 Berlin",
-#     cuisine: "German",
-#     phone_number: " 030 26369884",
-#     category: "restaurant",
-#     website: "https://www.joseph-roth-diele.de/",
-#     google_rating: "4.4",
-#     opening_times: "",
-#     photo_url: ""
-#   }
-# ]
 
 puts 'Creating users...'
 users = [
